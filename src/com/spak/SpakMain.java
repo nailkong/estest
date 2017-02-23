04f8bd3a9e785297634df0f90da73ff044828ba3
nailkong <qdkongjun@gmail>
2017-02-23 17:16:59 +0800

    second

--- src/com/spak/SpakMain.java
+++ src/com/spak/SpakMain.java
@@ -2,6 +2,10 @@ package com.spak;
 
 public class SpakMain {
 
+	public static void check(String str){
+		System.out.println(str);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello word");
